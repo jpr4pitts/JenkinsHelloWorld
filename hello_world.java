74c3f08953008e8b69de094e818c2baf246f5d53
John Repic <98906819+jpr4pitts@users.noreply.github.com>
2022-02-15 10:13:09 -0500

    updated helloworld

--- hello_world.java
+++ hello_world.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World and Welcome to Jenkins Training!"); 
+        System.out.println("Hello World!"); 
     }
 }
